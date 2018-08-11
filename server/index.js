25d816db3df080b2557fcecfcbfbd247942b9d66
jdpaterson <jaydpaterson@gmail.com>
2018-08-11 13:02:34 -0700

    Still setting up. Moved the foundation _settings file to client/scss folder. Modified the package.json to include build-css script. SASS compilation working properly

--- server/index.js
+++ server/index.js
@@ -3,11 +3,9 @@ const app = express()
 
 const path = require('path');
 const homePage = path.join(__dirname, '../client/index.html');
-const foundationSites = path.join(__dirname, '../node_modules/foundation-sites/dist/css');
 
 app.use(express.static('public'));
-app.use('/me', express.static(foundationSites));
 
-app.get('/', (req, res) => res.sendFile(homePage));
+app.get('/', (req, res) => res.sendFile('index.html'));
 
 app.listen(3000, () => console.log('jpdev-home listening on port 3000'))
