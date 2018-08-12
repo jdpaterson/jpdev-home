bdb0adab8fdebd95671b134b85035e8eb7a61b30
jdpaterson <jaydpaterson@gmail.com>
2018-08-12 08:35:04 -0700

    Migrated my resume template over including the foundation responsive timeline

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const homePage = path.join(__dirname, '../client/index.html');
 
 app.use(express.static('public'));
-
+app.use('/js', express.static(path.join(__dirname, '../node_modules/foundation-sites/dist/js')));
 app.get('/', (req, res) => res.sendFile('index.html'));
 
-app.listen(3000, () => console.log('jpdev-home listening on port 3000'))
+app.listen(3000, () => console.log('jpdev-home listening on port 3000'));
